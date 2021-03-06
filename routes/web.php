b8a35ef8e5f626d3e09a802d34edd678c106d5bf
vagrant <vagrant@homestead>
2021-03-06 08:32:29 +0000

    明明路由

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@
  * @version:
  * @Author: lmg
  * @Date: 2021-03-06 11:40:17
- * @LastEditTime: 2021-03-06 14:53:31
+ * @LastEditTime: 2021-03-06 16:30:00
  */
 
 use Illuminate\Support\Facades\Route;
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
-
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
