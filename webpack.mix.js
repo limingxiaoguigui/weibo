0bbd46f8a9781dfc07d2e21194b74992f64276ae
vagrant <vagrant@homestead>
2021-03-06 08:21:59 +0000

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,11 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: lmg
+ * @Date: 2021-03-06 11:40:18
+ * @LastEditTime: 2021-03-06 16:19:47
+ */
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+  .sass('resources/sass/app.scss', 'public/css').version();
