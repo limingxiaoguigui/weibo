8c0eae78035a625169caf25485fad66cf183b2bb
vagrant <vagrant@homestead>
2021-03-10 04:44:16 +0000

    数据库配置

---
+++ app/helpers.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * @Description:
+ * @version:
+ * @Author: lmg
+ * @Date: 2021-03-10 12:41:12
+ * @LastEditTime: 2021-03-10 12:41:23
+ */
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
